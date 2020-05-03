ac06a033de1a5193b03c4b434f9166b0b2307395
veerasolaiyappan <newlinedeveloper@gmail.com>
2020-05-03 12:39:12 +0530

    testing commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+
+      <div className="text-center" style={{ margin : "50px"}}>
+          <h2>Parkzap Assignment - User Registration Application</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
